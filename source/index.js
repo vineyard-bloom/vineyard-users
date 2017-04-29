1abef752ecb6d53758e49be03f19d84664ae9c7a
cj <christopherwjohnson@gmail.com>
2017-04-29 15:21:52 -0600

    Exporting User_Service from the index.

--- source/index.js
+++ source/index.js
@@ -2,7 +2,9 @@
 function __export(m) {
     for (var p in m) if (!exports.hasOwnProperty(p)) exports[p] = m[p];
 }
-__export(require('./User_Manager'));
-__export(require('./utility'));
-__export(require('./two-factor'));
+Object.defineProperty(exports, "__esModule", { value: true });
+__export(require("./User_Manager"));
+__export(require("./User_Service"));
+__export(require("./utility"));
+__export(require("./two-factor"));
 //# sourceMappingURL=index.js.map
--- source/index.ts
+++ source/index.ts
@@ -1,3 +1,4 @@
 export * from './User_Manager'
+export * from './User_Service'
 export * from './utility'
 export * from './two-factor'
