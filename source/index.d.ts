a16bc53fc20f66d3b9cb1e83dceee3f01db605ba
cj <cj@silentorb.com>
2017-07-08 19:11:19 -0600

    Renamed source to src.  Merged lab into this repo from a separate repo.

--- source/index.d.ts
+++
@@ -1,4 +0,0 @@
-export * from './user-manager';
-export * from './user-service';
-export * from './utility';
-export * from './two-factor';
--- source/utility.d.ts
+++
@@ -1,4 +0,0 @@
-export declare type Role_Id_Parameter = Role | number;
-export declare function has_role(user: User, role: Role_Id_Parameter): boolean;
-export declare function has_any_role(user: User, roles: Role_Id_Parameter[]): boolean;
-export declare function promiseEach(items: any, action: any): any;
