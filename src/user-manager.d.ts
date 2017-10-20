ac6cc02b9eef5c1cabfae82accf3f4c3b669cebf
cj <christopherwjohnson@gmail.com>
2017-10-20 12:54:56 -0600

    For now this beta version of vineyard-users is no longer using the beta version of ground.

--- src/user-manager.d.ts
+++ src/user-manager.d.ts
@@ -52,8 +52,8 @@ export declare class UserManager {
     createTempPassword(username: string | User): Promise<any>;
     createEmailCode(user: User): Promise<any>;
     verifyEmailCode(userId: string, submittedCode: string): Promise<boolean>;
-    getEmailCode(user: User): Promise<any>;
-    getTempPassword(user: User): Promise<any>;
+    getEmailCode(user: User): Promise<EmailVerification | undefined>;
+    getTempPassword(user: User): Promise<TempPassword | undefined>;
     getUserOneTimeCode(user: User): Promise<Onetimecode | undefined>;
     fieldExists(key: string, value: any): Promise<boolean>;
     compareOneTimeCode(oneTimeCode: string, codeRecord: Onetimecode | undefined): Promise<boolean>;
